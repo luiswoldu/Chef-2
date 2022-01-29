47b4b89fb5d65ac3776464b12a7650e1ffbe2403
luiswoldu <luiswoldu@googlemail.com>
2022-01-29 16:14:02 -0400

    Initial Commit

---
+++ Chef1.9/Chef1_9App.swift
@@ -0,0 +1,17 @@
+//
+//  Chef1_9App.swift
+//  Chef1.9
+//
+//  Created by Luis Woldu on 2022-01-29.
+//
+
+import SwiftUI
+
+@main
+struct Chef1_9App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
